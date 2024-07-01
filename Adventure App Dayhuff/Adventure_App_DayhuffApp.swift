638679a444cb61a3adf198a7019f285db02895d1
Cliffhang449 <noahdayhuff@gmail.com>
2024-07-01 18:41:12 -0500

    Initial Commit

---
+++ Adventure App Dayhuff/Adventure_App_DayhuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adventure_App_DayhuffApp.swift
+//  Adventure App Dayhuff
+//
+//  Created by Noah Dayhuff on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Adventure_App_DayhuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
